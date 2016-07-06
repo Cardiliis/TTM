e17155af31c985bcd855eddfb3fd0dae8eb71295
Cardiliis <francoisauger44@gmail.com>
2016-07-07 00:36:56 +0200

    add remote-sync and test class

---
+++ src/Domain/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TeamTimeManager\Domain;
+
+class User
+{
+    private
+        $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
